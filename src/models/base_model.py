191b525fe77a89df54f7dfc22314d648f684a281
lucastorresolist <lucas.torres@olist.com>
2021-01-29 16:58:47 -0300

    Removido validates sem necessidade

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -7,9 +7,3 @@ Base = declarative_base()
 class BaseModel(Base):
     __abstract__ = True
     id_ = Column("ID", Integer, nullable=False, primary_key=True)
-    
-    @validates("id_")
-    def validate_id_(self, key, id_: str):
-        if isinstance(id_, int) or not id_:
-            raise ValueError("Id must be int or none")
-        return id_
